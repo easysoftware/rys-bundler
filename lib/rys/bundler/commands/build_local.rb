b2863c361c164aaf34f88368766e85a5d116a58f
Ondřej Moravčík <moravcik.ondrej@gmail.com>
2018-06-18 15:05:55 +0200

    refs #252797 builder report and revert

--- lib/rys/bundler/commands/build_local.rb
+++ lib/rys/bundler/commands/build_local.rb
@@ -10,16 +10,16 @@ module Rys
         def build_gem_from_git(dependency, source, path)
           if source.send(:local?)
             path.make_symlink(source.path)
-            print_dependency_status(dependency, 'symlinked')
+            print_gem_status(dependency.name, 'symlinked')
           else
             command %{git clone --branch="#{source.branch}" "#{source.uri}" "#{path}"}
-            print_dependency_status(dependency, 'clonned')
+            print_gem_status(dependency.name, 'clonned')
           end
         end
 
         def build_gem_from_path(dependency, source, path)
           path.make_symlink(source.path)
-          print_dependency_status(dependency, 'symlinked')
+          print_gem_status(dependency.name, 'symlinked')
         end
 
       end
